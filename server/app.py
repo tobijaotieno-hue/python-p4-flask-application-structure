11714d2211a04059d193b742aa49583aa15a35a6
tobijaotieno-hue <tobijaotieno@gmail.com>
2026-01-12 16:00:20 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -1,14 +1,45 @@
+#!/usr/bin/env python3
+
 from flask import Flask
 
 app = Flask(__name__)
-
 @app.route('/')
 def index():
-    return '<h1>Welcome to my page!</h1>'
+    return"<h1>Python Operations with Flask Routing and Views</h1>"
 
-@app.route('/<string:username>')
-def user(username):
-    return f'<h1>Profile for {username}</h1>'
+@app.route('/print/<parameter>')
+def print_string(parameter):
+    print(parameter)
+    return parameter
+
+@app.route('/count/<parameter>')
+def count(parameter):
+    output=""
+
+    for i in range(int(parameter)):
+        print(i)
+        output += f'{i}\n'
+
+    return output
+
+@app.route("/math/<int:num1>/<operation>/<int:num2>")
+def math(num1, operation, num2):
+
+    if operation == "+":
+        return str(num1 + num2)
+
+    elif operation == "-":
+        return str(num1 - num2)
+
+    elif operation == "*":
+        return str(num1 * num2)
+
+    elif operation == "div":
+        return str(num1 / num2)
+
+    elif operation == "%":
+        return str(num1 % num2)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
